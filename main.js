53506afce5f2ebd0ba8b6ae756742c51340fe495
shoki-0303 <shoki522.33333@gmail.com>
2019-11-19 21:25:20 +0900

    homeControllerの呼び出し

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const layouts = require("express-ejs-layouts");
 
+//controllerの呼び出し
+const homeController = require("./controllers/homeController");
+
 //middlewareの設定
 app.set("view engine", "ejs");
 app.use(layouts);
