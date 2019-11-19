21df8a0f6eb31bfc0b6b0041871753694ae54d51
shoki-0303 <shoki522.33333@gmail.com>
2019-11-19 21:07:24 +0900

    set ejs

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const port = "3000";
 const express = require("express");
 const app = express();
 
+//middlewareの設定
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.send("hello world")
 });
