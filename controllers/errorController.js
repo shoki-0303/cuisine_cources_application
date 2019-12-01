68e9e9049eb9e309d28c54b61d68f1a79db50bff
shoki-0303 <shoki522.33333@gmail.com>
2019-12-01 18:32:15 +0900

    Refactor

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -3,12 +3,12 @@ const httpStatus = require("http-status-codes");
 exports.pageNotFoundError = (req, res) => {
     let errorCode = httpStatus.NOT_FOUND;
     res.status(errorCode);
-    res.render("404");
+    res.render("errors/404");
 };
 
 exports.internalServerError = (err, req, res, next) => {
     let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
     console.error(err.stack)
     res.status(errorCode);
-    res.render("500");
+    res.render("errors/500");
 };
