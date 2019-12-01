7d6233cd2396b766e730e41fd9e193c97d0f6593
shoki-0303 <shoki522.33333@gmail.com>
2019-12-01 18:31:24 +0900

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -3,7 +3,7 @@ const httpStatus = require("http-status-codes");
 exports.pageNotFoundError = (req, res) => {
     let errorCode = httpStatus.NOT_FOUND;
     res.status(errorCode);
-    res.send(`${errorCode}| the page does not exist`)
+    res.render("404")
 };
 
 exports.internalServerError = (err, req, res, next) => {
