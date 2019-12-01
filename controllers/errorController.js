02644a813e34aac8c104c94c00a05952734e5a2e
shoki-0303 <shoki522.33333@gmail.com>
2019-12-01 18:31:39 +0900

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -3,12 +3,12 @@ const httpStatus = require("http-status-codes");
 exports.pageNotFoundError = (req, res) => {
     let errorCode = httpStatus.NOT_FOUND;
     res.status(errorCode);
-    res.render("404")
+    res.render("404");
 };
 
 exports.internalServerError = (err, req, res, next) => {
     let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
     console.error(err.stack)
     res.status(errorCode);
-    res.send("500 error")
+    res.render("500");
 };
