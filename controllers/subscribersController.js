20c51e29cb7f5cebb9d95322c2624f622239590b
shoki-0303 <shoki522.33333@gmail.com>
2019-12-02 19:22:04 +0900

    saveSubscriber関数の定義

--- controllers/subscribersController.js
+++ controllers/subscribersController.js
@@ -10,4 +10,18 @@ exports.getAllsubscribers = (req, res, next) => {
 
 exports.getSubscriptionPage = (req, res) => {
     res.render("contact")
-}
+};
+
+exports.saveSubscriber = (req, res) => {
+    let newSubscriber = new Subscriber({
+        name: req.body.name,
+        email: req.body.email,
+        zipCode: req.body.zipCode
+    });
+
+    newSubscriber.save((error, document) => {
+        if (error) res.send(error);
+        let name = document.name;
+        res.render("thanks", {name: name});
+    });
+};
